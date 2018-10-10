e68f78a422203d37afd093cadc410eaed8842f86
Eduardo Gallego <eduardo.gallego@bluu.es>
2018-10-10 13:11:34 +0200

    no message

--- lessons-twitter/twitter/TwitterProducer.java
+++ lessons-twitter/twitter/TwitterProducer.java
@@ -146,10 +146,24 @@ public class TwitterProducer implements Runnable {
     }
 
     private KafkaProducer<String, String> createKafkaProducer() {
+        // Producer Properties
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Safe Producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        // High Throughput producer
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32768)); // 32 KB
+
+        // Create Producer
         return new KafkaProducer<>(properties);
     }
 
